a83a4baac239f98eda3bf9a1f0a52b0c00f63564
Pranav-S-S <pranav@graphenesvc.com>
2023-02-17 23:41:37 +0530

    NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import './index.css'
+import NavBar from './components/NavBar';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 function App() {
 	return (
-		<div className="App">
-			Helooo
+		<div className="App bg-[#212529] font-['Poppins']">
+			<BrowserRouter>
+				<div className="absolute z-50">
+					<NavBar />
+				</div>
+				<div className="h-screen w-full z-0 overflow-y-scroll scrollbar">
+					<div className="h-[300vh]"></div>
+					<Routes>
+						<Route index element={<Navigate to="/home" replace />} />
+						{/* <Route path="/requests" element={<Requests />} /> */}
+					</Routes>
+				</div>
+			</BrowserRouter>
 		</div>
 	);
 }
