a041caf35637d2f56321be54e30e79508f7d0d86
PranavSindhanuru <pranavsindhanuru@gmail.com>
2023-02-26 17:58:35 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './index.css'
 import NavBar from './components/NavBar';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { HashRouter , Navigate, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import Events from './pages/events';
 import Artists from './pages/artists';
@@ -9,21 +9,20 @@ import Contacts from './pages/contacts';
 function App() {
 	return (
 		<div className="App bg-[#212529] font-['Poppins'] ">
-			<BrowserRouter>
+			<HashRouter >
 				<div className="absolute z-50">
 					<NavBar />
 				</div>
 				<div className="h-screen w-screen z-0 overflow-y-scroll scrollbar pt-[5vh] text-[#F8F9FA] ">
 					<Routes>
 						<Route index element={<Navigate to="/home" replace />} />
-						<Route path='/dsu_fest_website' element={<Navigate to="/home" replace />} />
 						<Route path="/home" element={<Home />} />
 						<Route path="/events" element={<Events />} />
 						<Route path="/artists" element={<Artists />} />
 						<Route path="/contact" element={<Contacts />} />
 					</Routes>
 				</div>
-			</BrowserRouter>
+			</HashRouter >
 		</div>
 	);
 }
